5b27d8e672c58062923f0bb9a72ac3bf6eacc09b
Joel Höner <athre0z@zyantific.com>
2019-08-04 15:45:24 +0200

    Implemented most of the network proto

--- src/ClickHouse.jl
+++ src/ClickHouse.jl
@@ -1,6 +1,6 @@
 module ClickHouse
 
-import Base: UInt64
+import Base: UInt64, convert
 using HTTP
 
 export VarUInt
@@ -25,18 +25,20 @@ end
 
 chread(ctx::ReadCtx, ::Type{T}) where T <: Number = read(ctx.io, T)
 chread(ctx::ReadCtx, ::Type{Bool})::Bool = read(ctx.io, Bool)
+chread(ctx::ReadCtx, x::UInt64)::Array{UInt8, 1} = read(ctx.io, x)
 
 function chread(ctx::ReadCtx, ::Type{String})::String
-    len = read(ctx.io, VarUInt)
-    read(ctx.io, len) |> String
+    len = chread(ctx, VarUInt) |> UInt64
+    chread(ctx, len) |> String
 end
 
-chwrite(ctx::WriteCtx, x::Number)::Int64 = write(ctx.io, x)
-chwrite(ctx::WriteCtx, x::Bool)::Int64 = write(ctx.io, x)
+chwrite(ctx::WriteCtx, x::Number) = write(ctx.io, x)
+chwrite(ctx::WriteCtx, x::Bool) = write(ctx.io, x)
+chwrite(ctx::WriteCtx, x::Array{UInt8, 1}) = write(ctx.io, x)
 
-function chwrite(ctx::WriteCtx, x::String)::Int64
-    chwrite(ctx.io, length(x))
-    chwrite(ctx.io, Array{UInt8}(x))
+function chwrite(ctx::WriteCtx, x::String)
+    chwrite(ctx, x |> length |> VarUInt)
+    chwrite(ctx, x |> Array{UInt8})
 end
 
 # ============================================================================ #
@@ -49,16 +51,13 @@ VarUInt(x::Number) = reinterpret(VarUInt, UInt64(x))
 UInt64(x::VarUInt) = reinterpret(UInt64, x)
 Base.show(io::IO, x::VarUInt) = print(io, UInt64(x))
 
-function chwrite(ctx::WriteCtx, x::VarUInt)::Int64
-    i::Int64 = 1
+function chwrite(ctx::WriteCtx, x::VarUInt)
     mx::UInt64 = x
     while mx >= 0x80
         write(ctx.io, UInt8(mx & 0xFF) | 0x80)
         mx >>= 7
-        i += 1
     end
     write(ctx.io, UInt8(mx & 0xFF))
-    i
 end
 
 function chread(ctx::ReadCtx, ::Type{VarUInt})::VarUInt
@@ -84,20 +83,21 @@ end
 # [Parse helpers]                                                              #
 # ============================================================================ #
 
-function impl_chread_for_ty(ztruct::Type)::Function
-    arg_exprs = [:(chread(ctx, $ty)) for ty ∈ ztruct.types]
+function impl_chread_for_ty(ty::Type)::Function
+    arg_exprs = [:(chread(ctx, $ty)) for ty ∈ ty.types]
+    sym = split(ty.name |> string, '.')[end] |> Symbol
     reader = quote
-        function chread(ctx::ReadCtx, ::Type{$(ztruct)})::$(ztruct.name)
-            $ztruct($(arg_exprs...))
+        function chread(ctx::ReadCtx, ::Type{$sym})::$sym
+            $ty($(arg_exprs...))
         end
     end
     eval(reader)
 end
 
-function impl_chwrite_for_ty(ztruct::Type)::Function
-    write_stmts = [:(chwrite(ctx, x)) for name ∈ fieldnames(ztruct)]
+function impl_chwrite_for_ty(ty::Type)::Function
+    write_stmts = [:(chwrite(ctx, x.$name)) for name ∈ fieldnames(ty)]
     writer = quote
-        function chwrite(ctx::WriteCtx, x::$(ztruct))::Int64
+        function chwrite(ctx::WriteCtx, x::$(ty))
             $(write_stmts...)
         end
     end
@@ -105,71 +105,59 @@ function impl_chwrite_for_ty(ztruct::Type)::Function
 end
 
 # ============================================================================ #
-# [Server messages (wire format)]                                              #
+# [Shared messages (wire format)]                                              #
 # ============================================================================ #
 
-struct ServerInfo
-    name::String
-    major_ver::VarUInt
-    minor_ver::VarUInt
-    rev::VarUInt
-    timezone::String
-end
+const BLOCK_INFO_FIELD_STOP = UInt64(0)
+const BLOCK_INFO_FIELD_OVERFLOWS = UInt64(1)
+const BLOCK_INFO_FIELD_BUCKET_NUM = UInt64(2)
 
-impl_chread_for_ty(ServerInfo)
-
-struct ServerPong
-end
-
-impl_chread_for_ty(ServerPong)
-
-struct ServerProgress
-    rows::VarUInt
-    bytes::VarUInt
-    total_rows::VarUInt
-end
-
-impl_chread_for_ty(ServerProgress)
-
-struct ServerProfileInfo
-    rows::VarUInt
-    blocks::VarUInt
-    bytes::VarUInt
-    applied_limit::VarUInt
-    rows_before_limit::VarUInt
-    calc_rows_before_limit::VarUInt
-end
-
-impl_chread_for_ty(ServerProfileInfo)
-
-struct ServerException
-    rows::UInt32
-    name::String
-    message::String
-    strack_trace::String
-end
-
-impl_chread_for_ty(ServerException)
-
-struct ServerBlockInfo
-    unk1::VarUInt
+struct BlockInfo
     is_overflows::Bool
-    unk2::VarUInt
     bucket_num::Int32
-    unk3::VarUInt
+
+    BlockInfo() = new(false, -1)
+    BlockInfo(is_overflows, bucket_num) = new(is_overflows, bucket_num)
 end
 
-impl_chread_for_ty(ServerBlockInfo)
+function chread(ctx::ReadCtx, ::Type{BlockInfo})::BlockInfo
+    is_overflows = false
+    bucket_num = -1
 
-struct ServerEndOfStream
+    while (field_num = UInt64(chread(ctx, VarUInt))) != BLOCK_INFO_FIELD_STOP
+        if field_num == BLOCK_INFO_FIELD_OVERFLOWS
+            is_overflows = chread(ctx, Bool)
+        elseif field_num == BLOCK_INFO_FIELD_BUCKET_NUM
+            bucket_num = chread(ctx, Int32)
+        else
+            throw("Unknown block info field")
+        end
+    end
+
+    BlockInfo(is_overflows, bucket_num)
 end
 
-impl_chread_for_ty(ServerEndOfStream)
+function chwrite(ctx::WriteCtx, x::BlockInfo)
+    # This mirrors what the C++ client does.
+    chwrite(ctx, VarUInt(BLOCK_INFO_FIELD_OVERFLOWS))
+    chwrite(ctx, x.is_overflows)
+    chwrite(ctx, VarUInt(BLOCK_INFO_FIELD_BUCKET_NUM))
+    chwrite(ctx, x.bucket_num)
+    chwrite(ctx, VarUInt(BLOCK_INFO_FIELD_STOP))
+end
 
-struct ServerColumn
+struct Column
     name::String
-    type_name::String
+    type::String
     data::Any
+end
+
+struct Block
+    temp_table::String
+    block_info::BlockInfo
+    num_columns::VarUInt
+    num_rows::VarUInt
+    columns::Array{Column}
 end
 
 const COL_TY_MAP = Dict(
@@ -186,84 +174,161 @@ const COL_TY_MAP = Dict(
     "Int64" => Int64,
 
     "DateTime" => UInt8,
+    "String" => String,
 )
 
-function chread(ctx::ReadCtx, ::Type{ServerColumn})::ServerColumn
-    name = chread(ctx.io, String)
-    type_name = chread(ctx.io, String)
-    ty = COL_TY_MAP[type_name]
-    data = [read(ctx.io, ty) for _ ∈ 1:size]
-    ServerColumn(name, type_name, data)
+function read_col(ctx::ReadCtx, num_rows::VarUInt)::Column
+    name = chread(ctx, String)
+    type = chread(ctx, String)
+    ty = COL_TY_MAP[type]
+    data = [chread(ctx, ty) for _ ∈ 1:UInt64(num_rows)]
+    Column(name, type, data)
 end
 
-struct ServerBlock
-    info::ServerBlockInfo
-    num_cols::VarUInt
-    num_rows::VarUInt
-    cols::Array{ServerColumn}
+function chwrite(ctx::WriteCtx, x::Column)
+    chwrite(ctx, x.name)
+    chwrite(ctx, x.type)
+    for x ∈ x.data
+        chwrite(ctx, x)
+    end
 end
 
-function chread(ctx::ReadCtx, ::Type{ServerBlock})::ServerBlock
-    something = chread(ctx.io, String)
-    @show something
-    @assert !ctx.compress "Compression unsupported"
-    info = chread(ctx.io, ServerBlockInfo)
-    num_cols = chread(ctx.io, VarUInt)
-    num_rows = chread(ctx.io, VarUInt)
-    cols = [chread(ctx, ServerColumn) for _ ∈ 1:num_cols]
-    ServerBlock(info, num_cols, num_rows, cols)
+function chread(ctx::ReadCtx, ::Type{Block})::Block
+    temp_table = chread(ctx, String)
+    block_info = chread(ctx, BlockInfo)
+    num_columns = chread(ctx, VarUInt)
+    num_rows = chread(ctx, VarUInt)
+    columns = [read_col(ctx, num_rows) for _ ∈ 1:UInt64(num_columns)]
+    Block(temp_table, block_info, num_columns, num_rows, columns)
 end
+
+function chwrite(ctx::WriteCtx, x::Block)
+    chwrite(ctx, x.temp_table)
+    chwrite(ctx, x.block_info)
+    chwrite(ctx, x.num_columns)
+    chwrite(ctx, x.num_rows)
+    for x ∈ x.columns
+        chwrite(ctx, x)
+    end
+end
+
+# ============================================================================ #
+# [Server messages (wire format)]                                              #
+# ============================================================================ #
+
+struct ServerInfo
+    server_name::String
+    server_major_ver::VarUInt
+    server_minor_ver::VarUInt
+    server_rev::VarUInt
+
+    # DBMS_MIN_REVISION_WITH_SERVER_TIMEZONE
+    server_timezone::String
+
+    # DBMS_MIN_REVISION_WITH_SERVER_DISPLAY_NAME
+    server_display_name::String
+
+    # DBMS_MIN_REVISION_WITH_VERSION_PATCH
+    server_version_patch::VarUInt
+end
+
+impl_chread_for_ty(ServerInfo)
+
+struct ServerPong
+end
+
+impl_chread_for_ty(ServerPong)
+
+struct ServerProgress
+    rows::VarUInt
+    bytes::VarUInt
+    total_rows::VarUInt
+
+    # DBMS_MIN_REVISION_WITH_CLIENT_WRITE_INFO
+    written_rows::VarUInt
+    written_bytes::VarUInt
+end
+
+impl_chread_for_ty(ServerProgress)
+
+struct ServerProfileInfo
+    rows::VarUInt
+    blocks::VarUInt
+    bytes::VarUInt
+    applied_limit::Bool
+    rows_before_limit::VarUInt
+    calc_rows_before_limit::Bool
+end
+
+impl_chread_for_ty(ServerProfileInfo)
+
+struct ServerException
+    rows::UInt32
+    name::String
+    message::String
+    strack_trace::String
+end
+
+impl_chread_for_ty(ServerException)
+
+struct ServerEndOfStream
+end
+
+impl_chread_for_ty(ServerEndOfStream)
 
 # ============================================================================ #
 # [Client messages (wire format)]                                              #
 # ============================================================================ #
 
 struct ClientHello
+    client_name::String
+    client_dbms_ver_major::VarUInt
+    client_dbms_ver_minor::VarUInt
+    client_dbms_ver_rev::VarUInt
     database::String
     username::String
     password::String
 end
 
 impl_chwrite_for_ty(ClientHello)
+impl_chread_for_ty(ClientHello)
 
 struct ClientPing
 end
 
 impl_chwrite_for_ty(ClientPing)
+impl_chread_for_ty(ClientPing)
 
 struct ClientInfo
-    name::String
-    dbms_ver_minor::VarUInt
-    dbms_ver_major::VarUInt
-    dbms_ver_rev::VarUInt
+    query_kind::UInt8
+    initial_user::String
+    initial_query_id::String
+    initial_address_string::String
+    read_interface::UInt8
+    os_user::String
+    client_hostname::String
+    client_name::String
+    client_ver_major::VarUInt
+    client_ver_minor::VarUInt
+    client_rev::VarUInt
+    quota_key::String # DBMS_MIN_REVISION_WITH_QUOTA_KEY_IN_CLIENT_INFO
+    client_ver_patch::VarUInt # DBMS_MIN_REVISION_WITH_VERSION_PATCH
 end
 
 impl_chwrite_for_ty(ClientInfo)
-
-struct ClientData
-    temp_table::String
-    chunks::Any # TODO
-end
-
-impl_chwrite_for_ty(ClientData)
+impl_chread_for_ty(ClientInfo)
 
 struct ClientQuery
-    unk1::String
-    unk2::VarUInt
-    unk3::String
-    initial_query_id::String
-    iface_type::VarUInt
-    hostname1::String
-    hostname2::String
+    query_id::String
     client_info::ClientInfo
-    quota_key::String # new in rev 54060
     settings::String
-    compress_enable::Bool
-    sql::String
-    data::ClientData
+    query_stage::VarUInt
+    compression::VarUInt
+    query::String
 end
 
 impl_chwrite_for_ty(ClientQuery)
+impl_chread_for_ty(ClientQuery)
 
 # ============================================================================ #
 # [Opcodes]                                                                    #
@@ -272,7 +337,10 @@ impl_chwrite_for_ty(ClientQuery)
 const CLIENT_HELLO = UInt64(0)
 const CLIENT_QUERY = UInt64(1)
 const CLIENT_DATA = UInt64(2)
+const CLIENT_CANCEL = UInt64(3)
 const CLIENT_PING = UInt64(4)
+const CLIENT_TABLE_STATUS_REQ = UInt64(5)
+const CLIENT_KEEP_ALIVE = UInt64(6)
 
 const SERVER_HELLO = UInt64(0)
 const SERVER_DATA = UInt64(1)
@@ -283,44 +351,52 @@ const SERVER_END_OF_STREAM = UInt64(5)
 const SERVER_PROFILE_INFO = UInt64(6)
 const SERVER_TOTALS = UInt64(7)
 const SERVER_EXTREMES = UInt64(8)
+const SERVER_TABLES_STATUS_REPORT = UInt64(9)
+const SERVER_TABLES_LOG = UInt64(10)
+const SERVER_TABLE_COLUMNS = UInt64(11)
 
 # ============================================================================ #
 # [Message decoding]                                                           #
 # ============================================================================ #
 
-const OPCODE_TY_MAP = Dict(
+const SERVER_OPCODE_TY_MAP = Dict(
     SERVER_HELLO => ServerInfo,
     SERVER_PONG => ServerPong,
-    SERVER_PROGRESS => ServerPong,
+    SERVER_PROGRESS => ServerProgress,
     SERVER_PROFILE_INFO => ServerProfileInfo,
     SERVER_EXCEPTION => ServerException,
-    SERVER_DATA => ServerBlockInfo,
-    SERVER_TOTALS => ServerBlockInfo,
-    SERVER_EXTREMES => ServerBlockInfo,
+    SERVER_DATA => Block,
+    SERVER_TOTALS => Block,
+    SERVER_EXTREMES => Block,
     SERVER_END_OF_STREAM => ServerEndOfStream,
 )
 
-function read_packet(io::IO)::Any
+const CLIENT_OPCODE_TY_MAP = Dict(
+    CLIENT_HELLO => ClientHello,
+    CLIENT_QUERY => ClientQuery,
+    CLIENT_DATA => Block,
+    CLIENT_PING => ClientPing,
+)
+
+function read_packet(io::IO, opcode_map::Dict{UInt64, DataType})::Any
     ctx = ReadCtx(io, false)
     opcode = chread(ctx, VarUInt)
-    ty = OPCODE_TY_MAP[opcode]
+    ty = opcode_map[UInt64(opcode)]
     chread(ctx, ty)
 end
+
+read_server_packet(io::IO)::Any = read_packet(io, SERVER_OPCODE_TY_MAP)
+read_client_packet(io::IO)::Any = read_packet(io, CLIENT_OPCODE_TY_MAP)
 
 # ============================================================================ #
 # [Message encoding]                                                           #
 # ============================================================================ #
 
-const TY_OPCODE_MAP = Dict(
-    ClientHello => CLIENT_HELLO,
-    ClientData => CLIENT_DATA,
-    ClientPing => CLIENT_PING,
-    ClientQuery => CLIENT_QUERY,
-)
+const CLIENT_TY_OPCODE_MAP = Dict(v => k for (k, v) ∈ CLIENT_OPCODE_TY_MAP)
 
 function write_packet(io::IO, packet::Any)
     ctx = WriteCtx(io, false)
-    opcode = TY_OPCODE_MAP[typeof(packet)]
+    opcode = CLIENT_TY_OPCODE_MAP[typeof(packet)]
     chwrite(ctx, VarUInt(opcode))
     chwrite(ctx, packet)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Test
 using ClickHouse
+using Sockets
 
 @test begin
     io = IOBuffer([0xC2, 0x0A])
@@ -10,8 +11,129 @@ end
 @test begin
     io = IOBuffer(UInt8[], read=true, write=true, maxsize=10)
     ctx = ClickHouse.WriteCtx(io, false)
-    nb = ClickHouse.chwrite(ctx, VarUInt(100_500))
+    ClickHouse.chwrite(ctx, VarUInt(100_500))
     seek(ctx.io, 0)
-    actual = read(ctx.io, 3)
-    actual == [0x94, 0x91, 0x06] && nb == 3
+    read(ctx.io, 3) == [0x94, 0x91, 0x06]
 end
+
+@testset "Decode & re-encode client packets" begin
+    # This .bin file was extracted from a tcpdump captured from a session
+    # with the official ClickHouse command line client.
+    data = read(open("select1-client-query.bin"), 100_000, all = true)
+    io = IOBuffer(data)
+    packets = []
+
+    # Read packets.
+
+    packet = ClickHouse.read_client_packet(io)
+    push!(packets, packet)
+    @test typeof(packet) == ClickHouse.ClientHello
+
+    packet = ClickHouse.read_client_packet(io)
+    push!(packets, packet)
+    @test typeof(packet) == ClickHouse.ClientPing
+
+    packet = ClickHouse.read_client_packet(io)
+    push!(packets, packet)
+    @test typeof(packet) == ClickHouse.ClientQuery
+
+    packet = ClickHouse.read_client_packet(io)
+    push!(packets, packet)
+    @test typeof(packet) == ClickHouse.Block
+
+    @test eof(io)
+
+    # Re-encode them.
+
+    io = IOBuffer(UInt8[], write = true, read = true, maxsize = 100_000)
+    for packet ∈ packets
+        ClickHouse.write_packet(io, packet)
+    end
+
+    seek(io, 0)
+    reencoded_data = read(io, 100_000)
+
+    @test reencoded_data == data
+end
+
+@testset "Decode server packets" begin
+    io = open("select1-server-query-resp.bin")
+
+    packet = ClickHouse.read_server_packet(io)
+    @test typeof(packet) == ClickHouse.ServerInfo
+
+    packet = ClickHouse.read_server_packet(io)
+    @test typeof(packet) == ClickHouse.ServerPong
+
+    packet = ClickHouse.read_server_packet(io)
+    @test typeof(packet) == ClickHouse.Block
+
+    packet = ClickHouse.read_server_packet(io)
+    @test typeof(packet) == ClickHouse.Block
+
+    packet = ClickHouse.read_server_packet(io)
+    @test typeof(packet) == ClickHouse.ServerProfileInfo
+
+    packet = ClickHouse.read_server_packet(io)
+    @test typeof(packet) == ClickHouse.ServerProgress
+
+    packet = ClickHouse.read_server_packet(io)
+    @test typeof(packet) == ClickHouse.Block
+
+    packet = ClickHouse.read_server_packet(io)
+    @test typeof(packet) == ClickHouse.ServerEndOfStream
+
+    @test eof(io)
+end
+
+# @testset "SELECT 1; on localhost DB" begin
+#     sock = connect("localhost", 9000)
+#     hello = ClickHouse.ClientHello("Julia", 1, 1, 54423, "", "default", "")
+#     ClickHouse.write_packet(sock, hello)
+
+#     server_info = ClickHouse.read_server_packet(sock)
+#     @test typeof(server_info) == ClickHouse.ServerInfo
+#     @test server_info.server_name == "ClickHouse"
+
+#     ping = ClickHouse.ClientPing()
+#     ClickHouse.write_packet(sock, ping)
+
+#     pong = ClickHouse.read_server_packet(sock)
+#     @test typeof(pong) == ClickHouse.ServerPong
+
+#     query = ClickHouse.ClientQuery(
+#         "",
+#         ClickHouse.ClientInfo(
+#             0x01,
+#             "",
+#             "",
+#             "0.0.0.0:0",
+#             0x01,
+#             "",
+#             "6b88a142c2bc",
+#             "ClickHouse client",
+#             19,
+#             11,
+#             54423,
+#             "",
+#             2
+#         ),
+#         "",
+#         2,
+#         0,
+#         "SELECT 1",
+#     )
+#     ClickHouse.write_packet(sock, query)
+
+#     block = ClickHouse.Block("", ClickHouse.BlockInfo(), 0, 0, [])
+#     ClickHouse.write_packet(sock, block)
+
+#     packet = ClickHouse.read_server_packet(sock)
+#     @show packet
+
+#     packet = ClickHouse.read_server_packet(sock)
+#     @show packet
+
+#     packet = ClickHouse.read_server_packet(sock)
+#     @show packet
+# end
