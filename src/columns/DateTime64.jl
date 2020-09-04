34e801b81f55b628d6043e6ba3e3feae92f1f2d4
Alexandr Romanenko <waralex@gmail.com>
2020-09-05 00:49:08 +0300

    fix typo

--- src/columns/DateTime64.jl
+++ src/columns/DateTime64.jl
@@ -1,27 +1,27 @@
 is_ch_type(::Val{:DateTime64})  = true
-result_type(::Val{:DateTime64}, precission_str)  = Vector{DateTime}
+result_type(::Val{:DateTime64}, precision_str)  = Vector{DateTime}
 
 
 function read_col_data(sock::ClickHouseSock, num_rows::VarUInt, ::Val{:DateTime64},
-    precission_str::String)
-    precission = parse(Int, precission_str)
+    precision_str::String)
+    precision = parse(Int, precision_str)
     data = chread(sock, Vector{Int64}, num_rows)
 
-    #Maximum precission of DateTime is milliseconds
-    datetime_muliplier =10^3 / 10^precission
+    #Maximum precision of DateTime is milliseconds
+    datetime_muliplier =10^3 / 10^precision
 
     return DateTime(1970) .+ Millisecond.(round.(Int64, data .* datetime_muliplier))
 end
 
 function write_col_data(sock::ClickHouseSock,
                             data::AbstractVector{DateTime}, ::Val{:DateTime64},
-                            precission_str::String
+                            precision_str::String
                             )
 
-    precission = parse(Int, precission_str)
+    precision = parse(Int, precision_str)
 
-    #Maximum precission of DateTime is milliseconds
-    datetime_muliplier =10^3 / 10^precission
+    #Maximum precision of DateTime is milliseconds
+    datetime_muliplier =10^3 / 10^precision
 
 
     d = round.(Int64,
